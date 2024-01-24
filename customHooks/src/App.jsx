7bce7dac2bd30d35c231ee3b1a4e493f171fd4af
alif-786 <alipkamate1@gmail.com>
2024-01-24 11:36:38 +0530

    custom hook created

---
+++ customHooks/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h4 bg-violet-200 p-4 underline'>App</div>
+  )
+}
+
+export default App
---
+++ customHooks/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+const useCurrancyInfo = function (currency) {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+            .then((res) => res.json())
+            .then((res) => setData(res[currency]));
+        console.log(data)
+    }, [currency])
+    console.log(data)
+    return data;
+}
+
+export default useCurrancyInfo
