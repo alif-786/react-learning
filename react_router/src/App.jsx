d8113823d6f3c9d335935dda1906e8326e4f26db
alif-786 <alipkamate1@gmail.com>
2024-02-05 17:21:47 +0530

    "project initialised"

---
+++ react_router/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
+function App() {
+
+  return (
+    <div>
+      <Header/>
+      <h1 className='bg-black text-lime-600 p-4'>hello world </h1>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
