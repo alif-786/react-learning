af92672f2ce2420e2f879c400a5d3ab8bf0ce8ab
alif-786 <alipkamate1@gmail.com>
2024-02-13 21:01:05 +0530

    todo app initialised

---
+++ todo-app/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import {TodoProvider } from './context'
+import './App.css'
+
+function App() {
+  const [todo, setTodo] = useState([])
+
+  const addTodo = (todo) => {
+    // generated a randome id for the each todo with using the math.random()
+    const RandomId = Math.floor(Math.random() * 100000000000)
+
+    setTodo((prev) => [{ id: RandomId, ...todo }, ...prev])
+  }
+
+  return (
+    <TodoProivder value={{todos,addTodo,updateTodo,deleteTodo,toggleCompleted}}>
+   <div className="bg-[#18181b] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
+            </div>
+    </TodoProivder>
+  )
+}
+
+export default App
