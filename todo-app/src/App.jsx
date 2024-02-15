6cd1c0d5fd66b3dbf7f78ad0a62e00c3b3f185a9
alif-786 <alipkamate1@gmail.com>
2024-02-15 20:07:30 +0530

    todo app complete

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,56 +1,71 @@
-import { useState } from 'react'
-import {TodoProvider } from './context'
+import { useState, useEffect } from 'react'
+import {TodoProvider} from './context'
 import './App.css'
+import TodoForm from './components/TodoForm'
+import TodoItem from './components/TodoItem'
 
 function App() {
   const [todos, setTodos] = useState([])
 
-  // function for adding todo 
   const addTodo = (todo) => {
-    // generated a randome id for the each todo with using the math.random()
-    const RandomId = Math.floor(Math.random() * 100000000000)
-    setTodos((prev) => [{ id: RandomId, ...todo }, ...prev])
+    setTodos((prev) => [{id: Date.now(), ...todo}, ...prev] )
   }
 
-  // function for updating todo 
-  const updateTodo = (id,todo) => {
-    setTodos((prev) => prev.map((prevTodo) =>(prevTodo.id === id ? todo: prevTodo)))
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo )))
+
+    
   }
 
-  // functin for deleting todo 
   const deleteTodo = (id) => {
-    setTodos((prev)=> prev.filter((todo) => todo.id !==id ))
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
   }
 
-  // function for checking if the todo is complete or not
-  const toggleCompleted = (id) => {
-    setTodos((prev) => prev.map((prevTodo) =>(prevTodo.id === id ? {...prevTodo, isCompleted:!prevTodo.isCompleted}: prevTodo)))
+  const toggleComplete = (id) => {
+    //console.log(id);
+    setTodos((prev) => 
+    prev.map((prevTodo) => 
+      prevTodo.id === id ? { ...prevTodo, 
+        completed: !prevTodo.completed } : prevTodo))
   }
 
   useEffect(() => {
     const todos = JSON.parse(localStorage.getItem("todos"))
-    
+
     if (todos && todos.length > 0) {
       setTodos(todos)
     }
-  }, [third])
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
   
+
+
+
   return (
-
-    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleCompleted}}>
-
-   <div className="bg-[#18181b] min-h-screen py-8">
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+      <div className="bg-[#070f1b] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
+                        <TodoForm />
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        {todos.map((todo) => (
+                          <div key={todo.id}
+                          className='w-full'
+                          >
+                            <TodoItem todo={todo} />
+                          </div>
+                        ))}
                     </div>
                 </div>
             </div>
-  </TodoProvider>
+    </TodoProvider>
   )
 }
 
